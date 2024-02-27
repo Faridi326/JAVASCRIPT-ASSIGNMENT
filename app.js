d926f3b34379115e98b3db2bb6d979a3fb4d19c8
Faridi326 <154678989+Faridi326@users.noreply.github.com>
2024-02-27 02:10:49 -0800

    javascript assignments from chapter 1 to 4

---
+++ app.js
@@ -0,0 +1,11 @@
+
+var FirstName = " error ! please enter a valid password"
+alert(FirstName)
+var LastName = "WELCOME TO JS LANDING HAPPY CODING"
+alert(LastName)
+var Age = "WELCOME TO JS LAND"
+alert(Age)
+var Email = " HAPPY CODING PREVENT THIS PAGE FROM ADDITIONAL DIALOGS"
+alert(Email)
+var Password = " HELLO ... I CAN RUN JS THROUGH MY WEB BROWSER,S CONSOLE"
+alert(Password)
